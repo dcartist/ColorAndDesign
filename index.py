dd87b4b5ba9825611f933ddea88df60ec8eaa408
DCartist <pbannerman13@gmail.com>
2020-10-02 12:11:58 -0400

    Added routes for colors

--- index.py
+++ index.py
@@ -1,7 +1,31 @@
 from flask import Flask, jsonify, request
+import json
+
+with open('newColors.json') as colorlisting:
+  data = json.load(colorlisting)
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return "Colors"
+@app.route('/colors')
+def colors():
+    return jsonify(data)
+@app.route('/colors/<name>', methods=['GET'])
+def colornames(name=None):
+  if request.method =='GET':
+    if name:
+      # return(jsonify(data.name == name))
+      colorlisted = []
+      for color in data:
+        if name == color['name']:
+         colorlisted.append(color)
+      return(jsonify(colorlisted))
+      # return f"{name} is something"
+    else:
+      colorlisted = []
+      for color in data:
+        colorlisted.append(color)
+        return(jsonify(colorlisted))
+
 app.run(port=3000, debug=True)
